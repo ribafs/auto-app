97a52487c4911702720518fa90adf7bdd340e272
Bjverde <bjverde@yahoo.com.br>
2019-07-24 01:12:49 -0300

    :hammer: #1 change sgbd to select

--- step01.php
+++ step01.php
@@ -4,7 +4,7 @@
   <div class="col-sm-6">
   <h2><?php langHelper::showMsg('BD-INFO'); ?></h2>
   <form method="POST" action="">
-  <input type="hidden" id="formid" name="formid" value="database">
+    <input type="hidden" id="formid" name="formid" value="database">
     <div class="form-group">
       <input type="text" class="form-control" id="host" name="host" value="localhost" required>
     </div>
@@ -18,8 +18,13 @@
       <input type="password" class="form-control" id="pass" placeholder="<?php langHelper::showMsg('BD-PASS'); ?>" name="pass">
     </div>
     <div class="form-group">
-      <input type="sgbd" class="form-control" id="sgbd" name="sgbd" value="mysql">
-    </div>
+      <label for="dbms"><?php langHelper::showMsg('BD-DBMS'); ?></label>
+      <select class="form-control" id="dbms" required>
+        <option value="mysql">MySQL</option>
+        <option value="pgsql">PostgreSQL</option>
+        <option value="sqlite">SqLite</option>
+      </select>
+    </div>    
     <div class="form-group">
       <input type="text" class="form-control" id="port" name="port" value="3306">
     </div>
