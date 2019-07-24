e3f5fa76afefc738463b0fe6e3e314fa94330bae
Bjverde <bjverde@yahoo.com.br>
2019-07-24 00:42:51 -0300

    :hammer: #1 header html move

--- header.php
+++ header.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
-<html lang="pt">
+<?php langHelper::showHtmlLang(); ?>
 <head>
-    <title>PHP Automatic Application</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="./assets/css/style.css" rel="stylesheet" type="text/css" />
-    <style type="text/css">
-    .panel-footer {
-        padding: 0;
-        background: none;
-    }
-    </style>
+  <title><?php langHelper::showNameVersion(); ?></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
+<body>
--- index.php
+++ index.php
@@ -7,20 +7,7 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
   $_SESSION[SYSTEM_ACRONYM]['step']=0;
 }
 
-?>
-<!DOCTYPE html>
-<?php langHelper::showHtmlLang(); ?>
-<head>
-  <title><?php langHelper::showNameVersion(); ?></title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-<?php
+require_once __DIR__.'/header.php';
 require_once __DIR__.'/jumbotron.php';
 
 $formId = ArrayHelper::get($_REQUEST,'formid');
