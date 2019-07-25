a4f65a570139b79eea06a58d406340996b7a49f0
Bjverde <bjverde@yahoo.com.br>
2019-07-24 22:29:54 -0300

    :art: #1 melhoria do css

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,5 @@
-<div class="container footer text-center">
+<footer>
     <i><?php langHelper::showMsg('ADAPTED'); ?> <a href="https://ribafs.org" target="_blank">Ribamar FS</a></i>
-</div>
-<br>
+</footer>
 </body>
 </html>
--- view/step00.php
+++ view/step00.php
@@ -1,23 +1,24 @@
 <div class="container">
-<div class="row">
-  <div class="col-sm-3"></div>
-  <div class="col-sm-6">
-  <h2>Choose your language</h2>
-  <form method="POST" action="">
-    <input type="hidden" id="formid" name="formid" value="language">
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="language" id="en-us" value="en-us">
-        <label class="form-check-label" for="en-us">
-            English - <img src="assets/images/en.png" alt="Flag en" />
-        </label>
+  <div class="row">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-6">
+      <h2>Choose your language</h2>
+      <form method="POST" action="">
+        <input type="hidden" id="formid" name="formid" value="language">
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="language" id="en-us" value="en-us">
+            <label class="form-check-label" for="en-us">
+                English - <img src="assets/images/en.png" alt="Flag en" />
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="language" id="pt-br" value="pt-br" checked>
+            <label class="form-check-label" for="pt-br">
+                Brazilian Portuguese - <img src="assets/images/brasil.png" alt="Flag Brazil" />
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Next</button>
+      </form>
     </div>
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="language" id="pt-br" value="pt-br" checked>
-        <label class="form-check-label" for="pt-br">
-            Brazilian Portuguese - <img src="assets/images/brasil.png" alt="Flag Brazil" />
-        </label>
-    </div>
-    <button type="submit" class="btn btn-primary">Next</button>
-  </form>
   </div>
 </div>
