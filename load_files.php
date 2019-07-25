b9ff03216b17bd065aa9705dd2dd829dfbae1fb8
Bjverde <bjverde@yahoo.com.br>
2019-07-24 22:44:07 -0300

    :fire: #1 move file

--- load_files.php
+++ load_files.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/constants.php';
+require_once __DIR__.'/classes/constants.php';
 require_once __DIR__.'/classes/autoload_class_autoapp.php';
 require_once __DIR__.'/helpers/autoload_formdin_helper.php';
 
