f92fa0d8cf8f92901cd482630d76eadb652e9b77
Bjverde <bjverde@yahoo.com.br>
2019-07-24 22:45:34 -0300

    #1 session

--- classes/constants.php
+++ classes/constants.php
@@ -7,3 +7,14 @@ define('SYSTEM_NAME'    , 'PHP Automatic Application');
 define('SYSTEM_ACRONYM' , 'auto-app');
 define('SYSTEM_VERSION' , '1.2.0');
 
+$session_status = session_status();
+$session = $session_status !== PHP_SESSION_ACTIVE;
+if ($session) {
+    session_cache_expire(60);
+    session_start();
+    $_SESSION[SYSTEM_ACRONYM]['lang']='en-us';
+    $_SESSION[SYSTEM_ACRONYM]['step']=0;
+}
+
+
+
--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 require_once __DIR__.'/load_files.php';
-if (session_status() !== PHP_SESSION_ACTIVE) {
-  session_cache_expire(60);
-  session_start();
-  $_SESSION[SYSTEM_ACRONYM]['lang']='en-us';
-  $_SESSION[SYSTEM_ACRONYM]['step']=0;
-}
 
 require_once __DIR__.'/view/header.php';
-
 
 if(file_exists('./classes/connection.php')){
   require_once __DIR__.'/show_system.php';
