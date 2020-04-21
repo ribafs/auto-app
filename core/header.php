69b83599b093a53b50e96659c3986c76a3ec54b3
Ribamar FS <ribafs@gmail.com>
2020-04-20 22:22:59 -0300

    Quarto commit. Release 1.3

---
+++ core/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <title>Aplicativos com CRUDs Automáticos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="../assets/css/style.css" rel="stylesheet" type="text/css" />
+    <style type="text/css">
+    .panel-footer {
+        padding: 0;
+        background: none;
+    }
+    </style>
+</head>
